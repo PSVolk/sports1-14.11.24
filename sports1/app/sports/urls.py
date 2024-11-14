4f68db15d60e26065690fb8d6f272b40652e1db3
PSVolk <159309202+PSVolk@users.noreply.github.com>
2024-11-14 21:52:24 +0500

    Add files via upload

--- sports1/app/sports/urls.py
+++ sports1/app/sports/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('<str:sport_name>', views.sport),
+    path('daytime/', views.daytime),
 ]
--- sports1/app/sports/views.py
+++ sports1/app/sports/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-
+import datetime
+import time
 
 def index(request):
     template = loader.get_template("sports/index.html")
     context = {'title': 'Главная страница',
                'pages': [('football', 'Футбол'),
                          ('basketball', 'Баскетбол'),
-                         ('hockey', 'Хоккей')
+                         ('hockey', 'Хоккей'),
+                         ('daytime/', 'время')
                          ]}
     return HttpResponse(template.render(context, request))
 
@@ -16,5 +18,21 @@ def sport(request, sport_name):
     template = loader.get_template("sports/sport.html")
     context = {'title': sport_name,
                 'text': f'Новости {sport_name}'
+                         }
+    return HttpResponse(template.render(context, request))
+
+def daytime(request):
+    template = loader.get_template("sports/daytime.html")
+    if datetime.datetime.now().time().hour <= 6:
+        text = 'доброй ночи'
+    elif datetime.datetime.now().time().hour <= 12:
+        text = 'доброе утро'
+    elif datetime.datetime.now().time().hour <= 18:
+        text = 'добрый день'
+    else:
+        text = 'добрый вечер'
+    context = {'title': 'время',
+                'time': time.strftime("%H:%M:%S", time.localtime()),
+                'daytime': text
                          }
     return HttpResponse(template.render(context, request))
